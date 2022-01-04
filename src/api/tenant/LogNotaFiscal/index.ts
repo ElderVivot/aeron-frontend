80b80bc2668abb732c32d5050a2955627828a86c
ElderVivot <eldervivot@gmail.com>
2022-01-04 09:02:04 -0300

    feat: update to filter with between

--- src/api/tenant/LogNotaFiscal/index.ts
+++ src/api/tenant/LogNotaFiscal/index.ts
@@ -4,7 +4,7 @@ import { ILogNotaFiscal } from './ILogNotaFiscal'
 
 export const fetchDataLogNotaFiscal = async (tenant: string): Promise<ILogNotaFiscal[] | null> => {
     try {
-        const url = `${process.env.API_HOST}/log_nota_fiscal?dateStartDown=2021-12-01&dateEndDow=2021-12-31`
+        const url = `${process.env.API_HOST}/log_nota_fiscal?dateStartDownBetween=2021-12-01&dateEndDownBetween=2021-12-31`
         const result = await axios.get(url, { headers: { tenant } })
         return result.data
     } catch (error) {
