f16475d1f6f15e7c8cfe966f9d1409e095df9e65
ElderVivot <eldervivot@gmail.com>
2022-06-01 21:46:46 -0300

    feat: implement sub tow main

--- src/components/LogNotaFiscal/_utils.ts
+++ src/components/LogNotaFiscal/_utils.ts
@@ -10,4 +10,14 @@ export function getCompetenceMain (): string {
     return competence
 }
 
-export const competencesToDown = [getCompetenceMain(), '202202']
+export function getCompetenceMainSubTwo (): string {
+    const today = new Date()
+
+    const competence = today.getDate() >= 16
+        ? formatDate(new Date(today.getFullYear(), today.getMonth() - 1, 1), '', 'yyyyMM')
+        : formatDate(new Date(today.getFullYear(), today.getMonth() - 2, 1), '', 'yyyyMM')
+
+    return competence
+}
+
+export const competencesToDown = [getCompetenceMain(), getCompetenceMainSubTwo()]
